cacb98beda6460972bca04a27a271a2817cd866c
BtcDrak <btcdrak@gmail.com>
2014-08-07 14:57:24 +0100

    Network settings

--- networks.js
+++ networks.js
@@ -1,61 +1,53 @@
 var Put = require('bufferput');
 var buffertools = require('buffertools');
-var hex = function(hex) {
-  return new Buffer(hex, 'hex');
-};
+var hex = function(hex) {return new Buffer(hex, 'hex');};
 
 exports.livenet = {
   name: 'livenet',
-  magic: hex('f9beb4d9'),
-  addressVersion: 0x00,
-  privKeyVersion: 128,
-  P2SHVersion: 5,
+  magic: hex('0f68c6cb'),
+  addressVersion: 0x47,
+  privKeyVersion: 199,
+  P2SHVersion: 33,
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('6FE28C0AB6F1B372C1A6A246AE63F74F931E8365E15A089C68D6190000000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('A10B409A53A11D97790D97705AE3ABEA151533C08E1230980476991F00549B4E'),
+    merkle_root: hex('00522753002939C78659B4FDC6ED56C6B6AACDC7586FACF2F6ADA2012ED31703'),
     height: 0,
-    nonce: 2083236893,
+    nonce: 4016033,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1231006505,
-    bits: 486604799,
+    timestamp: 1405164774,
+    bits: 0x1e01ffff,
   },
   dnsSeeds: [
-    'seed.bitcoin.sipa.be',
-    'dnsseed.bluematt.me',
-    'dnsseed.bitcoin.dashjr.org',
-    'seed.bitcoinstats.com',
-    'seed.bitnodes.io',
-    'bitseed.xf2.org'
+    'seed.viacoin.net',
+    'mainnet.viacoin.net',
   ],
-  defaultClientPort: 8333
+  defaultClientPort: 5223
 };
-
-exports.mainnet = exports.livenet;
 
 exports.testnet = {
   name: 'testnet',
-  magic: hex('0b110907'),
-  addressVersion: 0x6f,
-  privKeyVersion: 239,
+  magic: hex('a9c5ef92'),
+  addressVersion: 127,
+  privKeyVersion: 255,
   P2SHVersion: 196,
   hkeyPublicVersion: 0x043587cf,
   hkeyPrivateVersion: 0x04358394,
   genesisBlock: {
-    hash: hex('43497FD7F826957108F4A30FD9CEC3AEBA79972084E90EAD01EA330900000000'),
-    merkle_root: hex('3BA3EDFD7A7B12B27AC72C3E67768F617FC81BC3888A51323A9FB8AA4B1E5E4A'),
+    hash: hex('7DB26CC9742EB385B7283AE6F67C7AE1D4E2B3F18D1CCCDECBFD08AA12A70A77'),
+    merkle_root: hex('00522753002939C78659B4FDC6ED56C6B6AACDC7586FACF2F6ADA2012ED31703'),
     height: 0,
-    nonce: 414098458,
+    nonce: 262929,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
-    timestamp: 1296688602,
-    bits: 486604799,
+    timestamp: 1405168772,
+    bits: 0x1e1fffff,
   },
   dnsSeeds: [
-    'testnet-seed.bitcoin.petertodd.org',
-    'testnet-seed.bluematt.me'
+    'seed-testnet.viacoin.net',
+    'testnet.viacoin.net'
   ],
-  defaultClientPort: 18333
+  defaultClientPort: 25223
 };
